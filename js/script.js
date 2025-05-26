e641d2dd0484f32094168a00eeda7b023623be9c
ramyr-sscgi <rlgarceniego@sscgi.com>
2025-05-26 09:57:58 +0800

    Update: added formspree template in index.html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Add custom JavaScript here
+function runTypingEffect() {
+  const text = "I am Ramyr Garcenigo.";
+  const typingEl = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingEl, typingDelay);
+}
+
+function typeText(text, typingEl, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingEl.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect());
